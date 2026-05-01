0bdf9f35e220f80b4521b37a1a1e3e8e9d42367b
it040 <23ituos040@ddu.ac.in>
2026-05-01 09:18:02 +0530

    Create model_defs.py

---
+++ ML-Model/model_defs.py
@@ -0,0 +1,17 @@
+﻿import torch
+import torch.nn as nn
+
+class AQILSTM(nn.Module):
+    def __init__(self, input_size=23, hidden_size=128, num_layers=3, output_size=7):
+        super(AQILSTM, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        out, _ = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
+        return out
